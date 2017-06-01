80c6ed116d09c97777d581610a5eca6dc6489383
Rômulo Farias <romulodefarias@gmail.com>
2017-05-31 22:54:52 -0300

    Update main to follow nfu logic

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
-/*
- * main.c
- *
- *  Created on: 3 de mai de 2017
- *      Author: romulo-farias
- */
+/******************************************************************************
+ * file: main.c 2017-05-31
+ * authors: Rômulo M. Farias and Felipe Pereira
+ * bachelor: Information Systems
+ *******************************************************************************/
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 #define PATH_FILE_1 "/home/romulo-farias/Documents/development/c/fifo_memory/resources/arquivo1.txt"
 #define PATH_FILE_2 "/home/romulo-farias/Documents/development/c/fifo_memory/resources/arquivo2.txt"
@@ -17,13 +17,16 @@
 struct node
 {
   int value;
+  unsigned age;
   struct node *next;
 }typedef list;
 
-void perform_fifo(int file_number, char *file_name, int pages_number);
-int exists(int value, list *memory);
-list* create_page(int value);
-list* last_page(list *memory);
+void perform_nfu(int file_number, char *file_name, int pages_number, int bits_num);
+void free_list(list *head);
+void age_list(list *head);
+list* older_page(list *head);
+list* exists(int value, list *head);
+list* create_page(int value, unsigned initial_age);
 list* read_file(char * file_name);
 
 int main(int argc, char **argv)
@@ -32,29 +35,33 @@ int main(int argc, char **argv)
 
   t1 = clock();
 
-  perform_fifo(1, PATH_FILE_1, 4);
-  perform_fifo(2, PATH_FILE_2, 4);
-  perform_fifo(3, PATH_FILE_3, 64);
-  perform_fifo(3, PATH_FILE_3, 256);
-  perform_fifo(3, PATH_FILE_3, 1024);
-  perform_fifo(3, PATH_FILE_3, 2048);
-  perform_fifo(4, PATH_FILE_4, 64);
-  perform_fifo(4, PATH_FILE_4, 256);
-  perform_fifo(4, PATH_FILE_4, 1024);
-  perform_fifo(4, PATH_FILE_4, 2048);
+  perform_nfu(1, PATH_FILE_1, 4, 4);
+//  perform_nfu(2, PATH_FILE_2, 4);
+//  perform_nfu(3, PATH_FILE_3, 64);
+//  perform_nfu(3, PATH_FILE_3, 256);
+//  perform_nfu(3, PATH_FILE_3, 1024);
+//  perform_nfu(3, PATH_FILE_3, 2048);
+//  perform_nfu(4, PATH_FILE_4, 64);
+//  perform_nfu(4, PATH_FILE_4, 256);
+//  perform_nfu(4, PATH_FILE_4, 1024);
+//  perform_nfu(4, PATH_FILE_4, 2048);
 
   t2 = clock();
 
-  printf("Total time: %d\n", t2 - t1);
+  printf("\n\nTotal time: %d\n", t2 - t1);
 
   return EXIT_SUCCESS;
 }
 
-void perform_fifo(int file_number, char *file_name, int pages_number)
+void perform_nfu(int file_number, char *file_name, int pages_number, int bits_num)
 {
+  unsigned initial_age = 1;
+  bits_num -= 1;
+  initial_age = initial_age << bits_num;
+
   list *memory = NULL;
-  list *last;
-  list *first;
+  list *tail;
+  list *head = NULL;
   list *removed_page;
   list *pages = read_file(file_name);
   int requests = 0;
@@ -62,11 +69,10 @@ void perform_fifo(int file_number, char *file_name, int pages_number)
   int page_faults = 0;
   int alocated_pages = 0;
 
-  if (pages == NULL)
-    return;
-
   while (pages != NULL)
   {
+    printf("\n");
+    age_list(head);
     page_value = pages->value;
     removed_page = pages;
     pages = pages->next;
@@ -74,72 +80,112 @@ void perform_fifo(int file_number, char *file_name, int pages_number)
 
     requests++;
 
-    if (memory == NULL)
+    if (head == NULL)
     {
-      memory = create_page(page_value);
+      head = create_page(page_value, initial_age);
+      tail = head;
       alocated_pages++;
       continue;
     }
 
-    if (exists(page_value, memory))
+    memory = exists(page_value, memory);
+
+    if (memory != NULL)
+    {
+      unsigned old = memory->age;
+      memory->age = memory->age | initial_age;
+      printf ("\n\n [ NEW ] page %d  is going from %d to %d! \n", memory->value, old, memory->age);
       continue;
+    }
 
     if (alocated_pages < pages_number)
     {
-      last = last_page(memory);
-      last->next = create_page(page_value);
+      tail->next = create_page(page_value, initial_age);
+      tail = tail->next;
       alocated_pages++;
     }
     else
     {
-      first = memory;
-      memory = first->next;
-      free(first);
-
-      last = last_page(memory);
-      last->next = create_page(page_value);
       page_faults++;
+      memory = older_page(head);
+      printf ("\n\n [ OLD (%d) ] %d wants to go in and %d is the oldest page (age = %d)\n", page_faults, page_value, memory->value, memory->age);
+      printf ("--------------------------------------------------------------------------------------\n");
+      memory->age = initial_age;
+      memory->value = page_value;
     }
   }
 
-  printf("Arquivo %d, %5d páginas, %7d requisições: FIFO: %7d Page Faults\n", file_number, pages_number, requests,
-      page_faults);
+  free_list(head);
+
+  printf ("%5d páginas, %7d requisições: NFU: %5d Page Faults\n", pages_number, requests, page_faults);
 }
 
-int exists(int value, list *memory)
+void free_list(list *head)
 {
-  list *node = memory;
+  list *memory;
+
+  while (head != NULL)
+  {
+    memory = head;
+    free(head);
+    head = memory->next;
+  }
+}
+
+void age_list(list *head)
+{
+  list *copy = head;
+
+  while (copy != NULL)
+  {
+    printf(" (Value:%d; Age:%d) ", copy->value, copy->age);
+
+    if (copy->age > 1)
+      copy->age = copy->age >> 1;
+    copy = copy->next;
+  }
+}
+
+
+list* older_page(list *head)
+{
+  list *copy = head;
+  list *older = head;
+
+  while (copy != NULL)
+  {
+    if (copy->age < older->age)
+    {
+      older = copy;
+    }
+
+    copy = copy->next;
+  }
+
+  return older;
+}
+
+list* exists(int value, list *head)
+{
+  list *node = head;
   while (node != NULL)
   {
     if (node->value == value)
-      return 1;
+      return node;
 
     node = node->next;
   }
 
-  return 0;
+  return NULL;
 }
 
-list* create_page(int value)
+list* create_page(int value, unsigned initial_age)
 {
   list *page = malloc(sizeof(list));
   page->value = value;
   page->next = NULL;
+  page->age = initial_age;
   return page;
-}
-
-list* last_page(list *memory)
-{
-  if (memory == NULL)
-    return NULL;
-
-  list *node = memory;
-  while (node->next != NULL)
-  {
-    node = node->next;
-  }
-
-  return node;
 }
 
 list* read_file(char *file_name)
